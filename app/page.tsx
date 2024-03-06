86320aaeede99bed4339853e78c275af047ab2d2
GVMafra <gvmafra@outlook.com>
2024-03-06 17:03:39 -0300

    add-shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 export default function Home() {
   return (
     <main className="flex flex-col md:flex-row h-screen items-center justify-between">
-      <div className="flex items-center justify-center border md:m-24 border-red-600 w-full h-screen md:w-[80vw] md:h-[80vh] md:left-24 md:min-h-2.5">
+      <div className="flex items-center justify-center md:m-24 shadow-inner bg-slate-300 rounded-xl w-full h-screen md:w-[80vw] md:h-[80vh] md:left-24 md:min-h-2.5">
         <div className="flex flex-wrap w-2/6 max-w-44 min-w-20 h-auto">
           <Image
-            src="/gvm_logo.svg"
-            alt="Vercel Logo"
+            src="/images/gvm_logo.svg"
+            alt="gvm logo"
             className="dark:invert w-auto h-auto"
             width={200}
             height={200}
@@ -16,7 +16,9 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="flex border border-red-600 w-full md:w-[20vw] md:h-screen"></div>
+      <div className="flex border bg-slate-300 w-full md:w-[20vw] md:h-screen">
+
+      </div>
     </main>
   );
 }
---
+++ components/sidebar.tsx
@@ -0,0 +1,14 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+
+//interface for sidebar contents
+
+
+
+const Sidebar = () => {
+
+}
