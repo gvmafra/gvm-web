f1b0dc29fd6e4122fa4d8384006b719a57008466
GVMafra <gvmafra@outlook.com>
2024-03-05 21:37:29 -0300

    last on tuesday

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import React from "react";
-import gvm_logo from "public/gvm_logo.svg";
 
 export default function Home() {
   return (
     <main className="flex flex-col md:flex-row h-screen items-center justify-between">
       <div className="flex items-center justify-center border md:m-24 border-red-600 w-full h-screen md:w-[80vw] md:h-[80vh] md:left-24 md:min-h-2.5">
-        <div className="flex flex-wrap w-2/6 h-auto">
+        <div className="flex flex-wrap w-2/6 max-w-44 min-w-20 h-auto">
           <Image
             src="/gvm_logo.svg"
             alt="Vercel Logo"
